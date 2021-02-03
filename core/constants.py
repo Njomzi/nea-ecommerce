b15b6caf8c1a9c32f4273a340722fda12df53ee6
Arta Teneqja <arta.teneqja@gmail.com>
2021-02-03 22:35:38 +0100

    Implement models

---
+++ core/constants.py
@@ -0,0 +1,17 @@
+CATEGORY_CHOICES = (
+    ('F', 'Football'),
+    ('B', 'Basketball'),
+    ('A', 'Auto-racing'),
+    ('Bo', 'Boxing'),
+    ('T', 'Të tjera')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primare'),
+    ('S', 'sekondare'),
+)
+
+ADDRESS_CHOICES = (
+    ('Fa', 'Faturimi'),
+    ('T', 'Transporti')
+)
